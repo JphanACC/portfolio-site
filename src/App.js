be4033292dfce36e24d35c956b7044cc851c32d6
Jack Phan <jphanacc@gmail.com>
2020-11-18 13:04:22 -0700

    added basic navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+//stylings
+import './App.scss';
+//components
+import Navbar from './components/Header/Navbar';
+
+// so what seems to be the problem?
+// the problem is in components -> Header -> Navbar
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar />
+      </div>
+    );
+  }
 }
 
 export default App;
